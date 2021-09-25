5dafa541ad706c6f01bbd80707ff3cfd4d5438a7
snapdeus <snapdeus@gmail.com>
2021-09-25 15:55:24 -0400

    finished login functionality

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,17 @@ router.get('/register', (req, res) => {
         ;
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.flash('success', 'Welcome To Yelp Camp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome To Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register')
@@ -29,7 +32,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds')
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res) => {
